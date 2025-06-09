1d36c552b524d22f6529ec22562e76f73c1d8395
Boladji <danyvinou@gmail.com>
2025-06-08 22:24:31 -0400

    fenwick tree done

---
+++ skiena/skiena/datastructures/trees/MyFenwickTree.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace skiena.datastructures.trees
+{
+    // https://cp-algorithms.com/data_structures/fenwick.html
+    public class MyFenwickTree
+    {
+        private List<long> partialSum;
+
+        public MyFenwickTree(int size)
+        {
+            partialSum = new List<long>(size);
+            for (int i = 0; i < size; i++)
+            {
+                partialSum.Add(0);
+            }
+        }
+        public MyFenwickTree(List<int> data)
+        {
+            partialSum = new List<long>(data.Count);
+            for (int i = 0; i < data.Count; i++) 
+            {
+                partialSum.Add(0);
+            }
+            for (int i = 0; i < data.Count; i++) 
+            {
+                partialSum[i] += data[i];
+                int tmp = h(i);
+                if (tmp < data.Count) 
+                {
+                    partialSum[tmp] += partialSum[i];
+                }
+            }
+        }
+
+        private int g(int idx) 
+        {
+            return idx & (idx + 1);
+        }
+        private int h(int idx) 
+        {
+            return idx | (idx + 1);
+        }
+
+        public void add(int idx,int val)
+        {
+            for (int i = idx; i < partialSum.Count; i = h(i)) 
+            {
+                partialSum[i] += val;
+            }
+        }
+        public long getSum(int idx)
+        {
+            long res = 0;
+            while (idx >= 0) 
+            {
+                res += partialSum[idx];
+                idx = g(idx) - 1;
+            }
+            return res;
+        }
+
+        public long getSum(int start, int end) 
+        {
+            return getSum(end) - getSum(start-1);
+        }
+    }
+}
---
+++ skiena/skienaTests/MyFenwickTreeTest.cs
@@ -0,0 +1,37 @@
+﻿using skiena.datastructures.trees;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace skienaTests
+{
+
+    [TestClass]
+    public class MyFenwickTreeTest
+    {
+        [TestMethod]
+        public void whenWeInitializeATreeWithData_thenTheSumShouldBeCalculated() 
+        {
+            List<int> data = new List<int>();
+            for (int i = 1; i < 11; i++)
+            {
+                data.Add(i);
+            }
+            MyFenwickTree tree = new MyFenwickTree(data);
+            Assert.AreEqual(5 * 6 / 2, tree.getSum(4));
+        }
+        [TestMethod]
+        public void whenWeInitializeATreeWithoutData_thenTheSumShouldBeCalculated()
+        {
+            MyFenwickTree tree = new MyFenwickTree(10);
+            for (int i = 0; i < 10; i++) 
+            {
+                tree.add(i, i+1);
+            }
+            
+            Assert.AreEqual(5 * 6 / 2, tree.getSum(4));
+        }
+    }
+}
