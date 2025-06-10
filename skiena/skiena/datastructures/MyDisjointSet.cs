0323d50a404edb61d6fe0d4b8b97b81a8c678fd2
Boladji <danyvinou@gmail.com>
2025-06-09 22:28:24 -0400

    my disjoint set done

---
+++ skiena/skiena/datastructures/MyDisjointSet.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace skiena.datastructures
+{
+    public class MyDisjointSet<T>
+    {
+        private Dictionary<T,T> roots = [];
+        private Dictionary<T,int> ranks = [];
+
+        public MyDisjointSet(T[] data) 
+        {
+            foreach (T node in data) 
+            {
+                insert(node);
+            }
+        }
+
+        public void insert(T node) 
+        {
+            roots.Add(node, node);
+            ranks.Add(node, 0);
+        }
+        public bool areConnected(T n1, T n2) 
+        {
+            return roots.ContainsKey(n1) && roots.ContainsKey(n2) && findRoot(n1).Equals(findRoot(n2));
+        }
+
+        public T findRoot(T node) 
+        {
+            T curr = node;
+            Stack<T> path = [];
+            while (roots.ContainsKey(curr) && !curr.Equals(roots[curr])) 
+            {
+                path.Push(curr);
+                curr = roots[curr];
+            }
+            while (path.Count > 0) 
+            {
+                roots[path.Pop()] = curr;
+            }
+            return curr;
+        }
+
+        public void connect(T node, T node2) 
+        {
+            var nodeParent = findRoot(node);
+            var node2Parent = findRoot(node2);
+            if (nodeParent != null && node2Parent !=null)
+            {
+                if (ranks[nodeParent] > ranks[node2Parent])
+                {
+                    roots[node2Parent] = nodeParent;
+                    ranks[nodeParent] += 1;
+                }
+                else
+                {
+                    roots[nodeParent] = node2Parent;
+                    ranks[node2Parent] += 1;
+                }
+            }
+        }
+    }
+}
---
+++ skiena/skienaTests/MyDisjointSetTests.cs
@@ -0,0 +1,37 @@
+﻿using skiena.datastructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace skienaTests
+{
+    [TestClass]
+    public class MyDisjointSetTests
+    {
+        [TestMethod]
+        public void whenTwoNodesAreConnected_ThenTheyShouldHaveSameRoot() 
+        {
+            int[] data = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            MyDisjointSet<int> disjointSet = new MyDisjointSet<int>(data);
+            disjointSet.connect(2, 10);
+
+            Assert.IsTrue(disjointSet.areConnected(2, 10));
+            Assert.AreEqual(disjointSet.findRoot(2), disjointSet.findRoot(10));
+        }
+        [TestMethod]
+        public void whenTwoNodesArentConnected_ThenTheyShouldNotHaveSameRoot()
+        {
+            int[] data = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            MyDisjointSet<int> disjointSet = new MyDisjointSet<int>(data);
+            disjointSet.connect(2, 10);
+            disjointSet.connect(1, 3);
+            disjointSet.connect(7, 6);
+            disjointSet.connect(8, 5);
+
+            Assert.IsTrue(disjointSet.areConnected(2, 10));
+            Assert.AreNotEqual(disjointSet.findRoot(4), disjointSet.findRoot(8));
+        }
+    }
+}
