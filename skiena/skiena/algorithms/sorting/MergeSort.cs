ba92fb7c0e51276d2d6cf45e4a50f61cc1734b5c
Boladji <danyvinou@gmail.com>
2025-07-19 12:06:20 -0400

    started working on sorting

---
+++ skiena/skiena/algorithms/sorting/MergeSort.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace skiena.algorithms.sorting
+{
+    public class MergeSort<T> where T : IEquatable<T>, IComparable<T>
+    {
+        public static void sort(List<T> data) 
+        {
+            mergeSort(data, 0, data.Count - 1);
+        }
+
+        private static void mergeSort(List<T> data, int start, int end)
+        {
+            if (start >= end) 
+            {
+                return;
+            }
+            int mid = start + (end - start) / 2;
+            mergeSort(data, start, mid);
+            mergeSort(data, mid + 1, end);
+            merge(data, start, mid, mid + 1, end);
+        }
+
+        private static void merge(List<T> data, int start1, int end1, int start2, int end2) 
+        {
+            T[] merged = new T[data.Count];
+            int i = start1;
+            int j = start2;
+            int count = 0;
+            for (; i <= end1 && j <= end2;) 
+            {
+                int comparison = data[i].CompareTo(data[j]);
+                if (comparison > 0)
+                {
+                    merged[count] = data[j];
+                    j++;
+                }
+                else
+                {
+                    merged[count] = data[i];
+                    ++i;
+                }
+                ++count;
+            }
+            while (i <= end1) 
+            {
+                merged[count] = data[i];
+                ++i;
+                ++count;
+            }
+            while (j <= end2)
+            {
+                merged[count] = data[j];
+                ++j;
+                ++count;
+            }
+            for (int k = 0; k < merged.Length; k++) 
+            {
+                data[k] = merged[k];
+            }
+        }
+    }
+}
---
+++ skiena/skiena/algorithms/sorting/Quicksort.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace skiena.algorithms.sorting
+{
+    public class Quicksort<T> where T : IEquatable<T>, IComparable<T>
+    {
+        public static void sort(List<T> data) 
+        {
+            quickSort(data, 0, data.Count-1);
+        }
+        private static void quickSort(List<T> data, int start, int end)
+        {
+            if (start >= end) 
+            {
+                return;
+            }
+            int pivot = start + (end-start)/2;
+            int left = pivot - 1;
+            int right = pivot + 1;
+            for (; left >=start && right <= end;) 
+            {
+                int leftComp = data[left].CompareTo(data[pivot]);
+                int rightComp = data[right].CompareTo(data[pivot]);
+                if (leftComp >= 0 && rightComp < 0)
+                {
+                    swap(data, left, right);
+                    --left;
+                    ++right;
+                }
+                else if (leftComp < 0)
+                {
+                    --left;
+                }
+                else if (rightComp >= 0) 
+                {
+                    ++right;
+                }
+            }
+            while (left >= start)
+            {
+                int leftComp = data[left].CompareTo(data[pivot]);
+                if (leftComp >= 0)
+                {
+                    int newPivot = pivot - 1;
+                    swap(data, left, pivot);
+                    if (newPivot >= start) 
+                    {
+                        swap(data, newPivot, left);
+                        pivot = newPivot;
+                    }
+                }
+                --left;
+            }
+            while (right <= end) 
+            {
+                int rightComp = data[right].CompareTo(data[pivot]);
+                if (rightComp < 0)
+                {
+                    int newPivot = pivot + 1;
+                    swap(data, right, pivot);
+                    if (newPivot <=end) 
+                    {
+                        swap(data, pivot, newPivot);
+                        pivot = newPivot;
+                    }
+                }
+                ++right;
+            }
+
+            quickSort(data, start, pivot);
+            quickSort(data, pivot + 1, end);
+        }
+        private static void swap(List<T> data, int i, int j) 
+        {
+            T tmp = data[i];
+            data[i] = data[j];
+            data[j] = tmp;
+        }
+    }
+}
