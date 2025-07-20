979cba87a86461b5e8aeb0835b27673071319489
Boladji <danyvinou@gmail.com>
2025-07-19 23:51:16 -0400

    radix sort coded

---
+++ skiena/skiena/algorithms/sorting/IDividableByInt.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace skiena.algorithms.sorting
+{
+    public interface IDivisibleByInt<T> :
+       IDivisionOperators<T, int, T>,
+       IDivisionOperators<T, long, T>,
+       IDivisionOperators<T, short, T>,
+       IDivisionOperators<T, ulong, T>,
+       IDivisionOperators<T, uint, T>,
+       IModulusOperators<T, int, T>,
+       IModulusOperators<T, long, T>,
+       IModulusOperators<T, short, T>,
+       IModulusOperators<T, ulong, T>,
+       IModulusOperators<T, uint, T>
+       where T : IDivisibleByInt<T>
+    {
+    }
+}
--- skiena/skiena/algorithms/sorting/RadixSort.cs
+++ skiena/skiena/algorithms/sorting/RadixSort.cs
@@ -7,7 +7,94 @@ using System.Threading.Tasks;
 
 namespace skiena.algorithms.sorting
 {
-    public class RadixSort<T> where T : IBinaryInteger<T>
+    public class RadixSort<T>  where T : IBinaryInteger<T>, IDivisibleByInt<T>
     {
+        public static void sort(List<T> data)
+        {
+            if (data.Count == 0) 
+            {
+                return;
+            }
+            T max = data.Max();
+            for (int exp = 1; (max/exp).CompareTo(0) != 0; exp *= 10) 
+            {
+                sortImpl(data, 0, data.Count - 1,exp);
+            }
+        }
+
+        private static void sortImpl(List<T> data, int start, int end, int exp)
+        {
+            T min = data[start];
+            T max = data[start];
+
+            List<T> all = new List<T>();
+            List<T> positives = new List<T>();
+            for (int i = start; i <= end; i++)
+            {
+                if (data[i].CompareTo(0) < 0)
+                {
+                    all.Add(data[i]);
+                }
+                else
+                {
+                    positives.Add(data[i]);
+                }
+            }
+            bool maxNegative = max.CompareTo(0) < 0;
+            if (maxNegative || (min.CompareTo(0) >= 0 && !maxNegative))
+            {
+                sameSignCountingSort(data, start, end,exp, maxNegative);
+            }
+            else
+            {
+                int mid = all.Count;
+                all.AddRange(positives);
+                for (int i = start; i <= end; i++)
+                {
+                    data[i] = all[i - start];
+                }
+                sameSignCountingSort(data, start, start + mid - 1, exp, true);
+                sameSignCountingSort(data, mid, end, exp, false);
+            }
+        }
+        private static void sameSignCountingSort(List<T> data, int start, int end, int exp, bool isNegative)
+        {
+            if (start >= end)
+            {
+                return;
+            }
+            int[] count = new int[10];
+            for (int i = 0; i < data.Count; i++)
+            {
+                ++count[computeCountIndex(data[i], exp, isNegative)];
+            }
+            for (int i = 1; i < count.Length; i++)
+            {
+                count[i] += count[i - 1];
+            }
+            T[] result = new T[data.Count];
+            for (int j = data.Count - 1; j >= 0; j--)
+            {
+                int idx = computeCountIndex(data[j], exp, isNegative);
+                --count[idx];
+                result[count[idx]] = data[j];
+            }
+            for (int i = 0; i < data.Count; i++)
+            {
+                data[i] = isNegative ? result[data.Count - i - 1] : result[i];
+            }
+        }
+        private static int computeCountIndex(T value, int exp, bool isNegative)
+        {
+            return (int)(object)(isNegative ? ((-value) / exp % 10 ): (value / exp) % 10);
+        }
+        private static int computeSpan(T min, T max, bool isNegative)
+        {
+            return isNegative ? (-min).CompareTo(-max) + 1 : max.CompareTo(min) + 1;
+        }
+        private static T getValue(T value, int exp) 
+        {
+            return (value / exp) % 10;
+        }
     }
 }
