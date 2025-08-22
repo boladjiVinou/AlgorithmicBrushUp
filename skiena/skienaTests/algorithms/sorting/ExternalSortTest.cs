8933e0cf0c0fd00c557be59b438c5fe8886aa26e
Boladji <danyvinou@gmail.com>
2025-08-21 22:15:38 -0400

    external sort test fix

--- skiena/skienaTests/algorithms/sorting/ExternalSortTest.cs
+++ skiena/skienaTests/algorithms/sorting/ExternalSortTest.cs
@@ -8,14 +8,14 @@ using System.Threading.Tasks;
 namespace skienaTests.algorithms.sorting
 {
     [TestClass]
-    public class ExternalSortTest
+    public class ExternalSortTest:BaseSortTest
     {
         [TestMethod]
         public void whenTryingToSortFileOfInt_thenWeShouldGenerateFileWithSortedData() 
         {
+            deleteTempFiles(".");
+
             Random random = new Random();
-            File.Delete(@".\data.txt");
-            File.Delete(@".\result.txt");
 
             int size = 1000;
             int parts = 10;
@@ -26,6 +26,7 @@ namespace skienaTests.algorithms.sorting
             Assert.IsTrue(File.Exists(@".\result.txt"));
             var result = ExternalSort<int>.enumerateData(@".\result.txt");
             Assert.IsTrue(result.Any());
+
             int prev = int.MinValue;
             foreach (var elem in result) 
             {
@@ -33,8 +34,35 @@ namespace skienaTests.algorithms.sorting
                 prev = elem;
             }
 
-            File.Delete(@".\data.txt");
-            File.Delete(@".\result.txt");
+            deleteTempFiles(".");
+        }
+
+        protected override void sort(List<int> data)
+        {
+            int size = data.Count;
+            var folder = $@".\{Guid.NewGuid().ToString()}";
+            Directory.CreateDirectory(folder);
+            File.WriteAllLines($@"{folder}\data.txt", data.Select(x => x.ToString()));
+
+            ExternalSort<int>.sort($@"{folder}\data.txt", $@"{folder}\", $@"{folder}\result.txt", Math.Min(5, size));
+            Assert.IsTrue(File.Exists($@"{folder}\result.txt"));
+            var result = ExternalSort<int>.enumerateData($@"{folder}\result.txt");
+            data.Clear();
+            data.AddRange(result);
+
+            Directory.Delete(folder, true);
+        }
+
+        private static void deleteTempFiles(string baseFolder)
+        {
+            if (File.Exists($@"{baseFolder}\data.txt"))
+            {
+                File.Delete($@"{baseFolder}\data.txt");
+            }
+            if (File.Exists($@"{baseFolder}\data.txt"))
+            {
+                File.Delete($@"{baseFolder}\result.txt");
+            }
         }
     }
 }
