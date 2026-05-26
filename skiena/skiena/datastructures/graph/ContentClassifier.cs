785d25440c1829ddc0a75acf2ff75f34d7d491d6
Boladji <danyvinou@gmail.com>
2026-05-25 23:58:15 -0400

    started working on articulation detection

--- skiena/skiena/datastructures/graph/ContentClassifier.cs
+++ skiena/skiena/datastructures/graph/ContentClassifier.cs
@@ -11,7 +11,10 @@ namespace skiena.datastructures.graph
     {
         private Dictionary<GraphNode<T>, Dictionary<GraphNode<T>, enEdge>> edgeTypeByEdge = [];
         private Dictionary<GraphNode<T>, int> entryTimePerNode = [];
+        private Dictionary<GraphNode<T>, GraphNode<T>> reachableAncestorByNode = [];
+        private Dictionary<GraphNode<T>, int> treeOutDegreeByNode = [];
         private Dictionary<GraphNode<T>, GraphNode<T>> parentByNode = [];
+        private Dictionary<GraphNode<T>, enArticulationNode> articulationByNode = [];
         private int time = 0;
         public override void preVisitNode(GraphNode<T> node)
         {
@@ -25,6 +28,14 @@ namespace skiena.datastructures.graph
             {
                 entryTimePerNode[node] = time;
             }
+            if (!reachableAncestorByNode.ContainsKey(node)) 
+            {
+                reachableAncestorByNode.Add(node,node);
+            }
+            if (!treeOutDegreeByNode.ContainsKey(node)) 
+            {
+                treeOutDegreeByNode.Add(node,0);
+            }
         }
 
         public override void visitNode(GraphNode<T> node)
@@ -33,11 +44,32 @@ namespace skiena.datastructures.graph
             {
                 updateEdgeType(node, neighbor);
 
+                updateNodeData(node, neighbor);
+
                 if (visitInProgress.Contains(neighbor) || visited.Contains(neighbor))
                 {
                     continue;
                 }
+                if (!parentByNode.ContainsKey(neighbor)) 
+                {
+                    parentByNode.Add(neighbor, node);
+                }
                 neighbor.accept(this);
+            }
+        }
+
+        private void updateNodeData(GraphNode<T> node, GraphNode<T> neighbor)
+        {
+            if (getEdgeType(node, neighbor) == enEdge.Tree)
+            {
+                ++treeOutDegreeByNode[node];
+            }
+            else if (getEdgeType(node, neighbor) == enEdge.Back && parentByNode.ContainsKey(node) && parentByNode[node] != neighbor)
+            {
+                if (entryTimePerNode[neighbor] < entryTimePerNode[reachableAncestorByNode[node]])
+                {
+                    reachableAncestorByNode[node] = neighbor;
+                }
             }
         }
 
@@ -85,6 +117,40 @@ namespace skiena.datastructures.graph
                 edgeTypeByEdge.Add(n1, []);
             }
             edgeTypeByEdge[n1].Add(n2, edge);
+        }
+
+        public override void postVisitNode(GraphNode<T> node)
+        {
+            base.postVisitNode(node);
+            if (!parentByNode.ContainsKey(node) && treeOutDegreeByNode.ContainsKey(node) && treeOutDegreeByNode[node] > 1) // root
+            {
+                articulationByNode.Add(node, enArticulationNode.RootArticulation);
+            }
+            GraphNode<T>? grandParent = null;
+            GraphNode<T>? parent = null;
+            if (parentByNode.ContainsKey(node))
+            {
+                parent = parentByNode[node];
+            }
+
+            if (parent != null && parentByNode.ContainsKey(parent)) 
+            {
+                grandParent = parentByNode[parentByNode[node]];
+            }
+            if (reachableAncestorByNode[node] == parent && grandParent != null)
+            {
+                articulationByNode.Add(node, enArticulationNode.ParentArticulation);
+            }
+
+            if(reachableAncestorByNode.ContainsKey(node) && reachableAncestorByNode[node] == node && treeOutDegreeByNode[node] > 0)
+            {
+                articulationByNode.Add(node, enArticulationNode.BridgeArticulation);
+            }
+            if (entryTimePerNode.ContainsKey(node) && parent != null && entryTimePerNode.ContainsKey(parent)
+                && entryTimePerNode[node] < entryTimePerNode[parent]) 
+            {
+                reachableAncestorByNode[parent] = reachableAncestorByNode[node];
+            }
         }
     }
 }
